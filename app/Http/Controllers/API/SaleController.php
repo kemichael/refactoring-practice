d0bddc22cd9622fc14411d3ecbedec4276b032be
ReiyaMasumoto <g.apple0323@gmail.com>
2025-03-21 15:24:01 +0900

    課題対応

--- app/Http/Controllers/API/SaleController.php
+++ app/Http/Controllers/API/SaleController.php
@@ -7,57 +7,20 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Sale;
 use App\Models\Product;
+use App\Http\Services\SaleService;
 
 class SaleController extends Controller
 {
+
+    protected $saleService;
+
+    public function _construct(SaleService $saleService) {
+        $this->saleService = $saleService;
+    }
+
     public function buy(Request $request){
-
-
-        $id = $request->input('product_id');
-
-        $product = DB::table('products')
-            ->join('companies', 'products.company_id', '=', 'companies.id')
-            ->select('products.*', 'companies.company_name')
-            ->where('products.id', '=', $id)
-            ->first();
-
-        //商品なし
-        if(!$product){
-            return response()->json('商品がありません');
-        }
-        //在庫なし
-        if($product->stock <= 0){
-            return response()->json('在庫がありません');
-        }
-
-        try {
-            DB::beginTransaction();
-            //productsテーブルのstock減算
-            DB::table('products')
-                ->where('id', '=', $id)
-                ->decrement('stock');
-        
-            //減算後の情報を返却
-            $afterBuy = DB::table('products')
-                ->select('id','product_name','stock')
-                ->where('id', '=', $id)
-                ->first();
-            
-            //salesテーブルにインサート
-            DB::table('sales')
-                ->insert([
-                    'product_id' => $id,
-                    'created_at' => now(),
-                    'updated_at' => now()
-                ]);
-            
-            DB::commit();
-        } catch (Throwable $e) {
-            DB::rollBack();
-        }
-
         //購入処理後の情報を返却
-        return response()->json($afterBuy);
+        return $this->saleService->buy($request);
         
     }
 }
